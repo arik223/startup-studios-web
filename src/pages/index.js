6e39db8b263ddecfcb79d7c4b6caff9ed4bedcc4
Ari Shaposhnik <arishaposhnik@Aris-MBP.the-beach.net>
2021-05-13 14:52:40 -0400

    New index

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,9 @@ import Image from '../components/image'
 import SEO from '../components/seo'
 
 import { makeStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+
 import Grid from '@material-ui/core/Grid'
 import Divider from '@material-ui/core/Divider'
 import ListSubheader from '@material-ui/core/ListSubheader'
@@ -12,13 +15,22 @@ import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
-import Collapse from '@material-ui/core/Collapse'
+import Hidden from '@material-ui/core/Hidden'
+
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
 import StarIcon from '@material-ui/icons/Star'
 import InfoIcon from '@material-ui/icons/Info'
 import ExpandLess from '@material-ui/icons/ExpandLess'
 import ExpandMore from '@material-ui/icons/ExpandMore'
-import Done from '@material-ui/icons/Done'
+import MailIcon from '@material-ui/icons/Mail'
+import { graphql, useStaticQuery } from 'gatsby'
+// import styled from 'styled-components'
+
+import BackgroundImage from 'gatsby-background-image'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import withWidth, { isWidthUp } from '@material-ui/core/withWidth'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -32,110 +44,244 @@ const useStyles = makeStyles(theme => ({
   nested: {
     paddingLeft: theme.spacing(4),
   },
+  background: {
+    width: '100%',
+    backgroundPosition: 'bottom center',
+    backgroundRepeat: 'repeat-y',
+    backgroundSize: 'cover',
+  },
+  grid: {
+    height: '85vh',
+    alignItems: 'center',
+    // textAlign: 'center',
+    width: '35%',
+    marginLeft: 20,
+    [theme.breakpoints.down('md')]: {
+      height: '95vh',
+    },
+  },
+  gridMob: {
+    height: '80vh',
+    alignItems: 'center',
+    textAlign: 'center',
+    width: '100%',
+    paddingLeft: 0,
+  },
 }))
 
-const IndexPage = () => {
+const IndexPage = props => {
   const classes = useStyles()
   const [features, setFeatures] = React.useState(true)
   const [info, setInfo] = React.useState(true)
 
-  function handleClick(id) {
-    switch (id) {
-      case 'features':
-        setFeatures(!features)
-        break
-      case 'info':
-        setInfo(!info)
-        break
-    }
+  const { mobileImage, desktopImage } = useStaticQuery(
+    graphql`
+      query {
+        mobileImage: file(relativePath: { eq: "bg-mob-min.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 490, quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+        desktopImage: file(relativePath: { eq: "bg-min.png" }) {
+          childImageSharp {
+            fluid(quality: 100, maxWidth: 4160) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `
+  )
+  // Set up the array of image data and `media` keys.
+  // You can have as many entries as you'd like.
+  const sources = [
+    mobileImage.childImageSharp.fluid,
+    {
+      ...desktopImage.childImageSharp.fluid,
+      media: `(min-width: 491px)`,
+    },
+  ]
+
+  // const data = useStaticQuery(
+  //   graphql`
+  //     query {
+  //       desktop: file(relativePath: { eq: "bg-min.png" }) {
+  //         childImageSharp {
+  //           fluid(quality: 90, maxWidth: 1920) {
+  //             ...GatsbyImageSharpFluid_withWebp
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `
+  // )
+
+  // Set ImageData.
+  let imageData
+  let mobile = false
+  if (isWidthUp('md', props.width)) {
+    imageData = desktopImage.childImageSharp.fluid
+  } else {
+    mobile = true
+    imageData = mobileImage.childImageSharp.fluid
   }
 
+  // const imageData = mobileImage.childImageSharp.fluid
+  // console.log(imageData)
   return (
-    <Layout>
-      <SEO title="Home" />
-      <Grid container spacing={3} justify="center">
-        <Grid item xs={2}>
-          <div style={{ maxWidth: `100px`, marginBottom: `1.45rem` }}>
-            <Image />
-          </div>
-        </Grid>
-        <Grid item xs={8}>
-          <h1>Gatsby Material UI Starter</h1>
-          <h5>
-            A responsive, minimalist Gatsby starter based on the world's most
-            popular React UI framework.
-          </h5>
-        </Grid>
-      </Grid>
-      <Divider />
-      <List component="nav" className={classes.root}>
-        <ListItem id="features" button onClick={() => handleClick('features')}>
-          <ListItemIcon>
-            <StarIcon />
-          </ListItemIcon>
-          <ListItemText primary="Features" />
-          {features ? <ExpandLess /> : <ExpandMore />}
-        </ListItem>
-        <Collapse in={!features} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon>
-                <Done />
-              </ListItemIcon>
-              <ListItemText primary="Material UI Framework" />
-            </ListItem>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon>
-                <Done />
-              </ListItemIcon>
-              <ListItemText primary="Progressive Web App" />
-            </ListItem>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon>
-                <Done />
-              </ListItemIcon>
-              <ListItemText primary="SEO" />
-            </ListItem>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon>
-                <Done />
-              </ListItemIcon>
-              <ListItemText primary="Offline Support" />
-            </ListItem>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon>
-                <Done />
-              </ListItemIcon>
-              <ListItemText primary="Roboto Typeface (self hosted)" />
-            </ListItem>
-          </List>
-        </Collapse>
-        <ListItem button onClick={() => handleClick('info')}>
-          <ListItemIcon>
-            <InfoIcon />
-          </ListItemIcon>
-          <ListItemText primary="Info" />
-          {info ? <ExpandLess /> : <ExpandMore />}
-        </ListItem>
-        <Collapse in={!info} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon>
-                <Done />
-              </ListItemIcon>
-              <ListItemText primary="Based on Gatsby Default Starter" />
-            </ListItem>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon>
-                <Done />
-              </ListItemIcon>
-              <ListItemText primary="Uses Gatsby Material UI Plugin" />
-            </ListItem>
-          </List>
-        </Collapse>
-      </List>
-    </Layout>
+    <BackgroundImage
+      Tag="div"
+      fluid={imageData}
+      style={{
+        // background: 'no-repeat bottom center fixed',
+        webkitBackgroundSize: 'cover',
+        mozBackgroundSize: 'cover',
+        oBackgroundSize: 'cover',
+        backgroundSize: 'cover',
+        backgroundPosition: mobile ? 'bottom center' : 'center',
+        // backgroundRepeat: 'repeat-y',
+        // backgroundSize: 'cover',
+        // overflow: 'none',
+      }}
+    >
+      <Layout header={false}>
+        <SEO title="Home" />
+        <Hidden mdUp>
+          <Grid
+            container
+            justify="center"
+            textAlign="center"
+            justifyContent="center"
+            className={classes.gridMob}
+          >
+            <Grid item>
+              <Box
+                style={{
+                  // padding: 35,
+                  backdropFilter: 'blur(5px)',
+                  // border: '2px solid black',
+                  borderRadius: 8,
+                  marginBottom: 20,
+                }}
+              >
+                <Typography variant="h2" style={{ color: 'white' }}>
+                  Be part of the future of innovative business collaboration.
+                  Join our startup studios today!
+                </Typography>
+                <Typography variant="h3">
+                  Type your email below to join our newsletter!
+                </Typography>
+              </Box>
+              <Grid container direction="row" spacing={2} justify="center">
+                <Grid item>
+                  <TextField
+                    id="outlined-basic"
+                    label="Email address"
+                    variant="outlined"
+                    style={{
+                      width: 302,
+                    }}
+                    InputProps={{
+                      endAdornment: <MailIcon />,
+                    }}
+                  />
+                </Grid>
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    style={{ height: '100%' }}
+                  >
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Hidden>
+
+        <Hidden smDown>
+          <Grid container justify="left" className={classes.grid}>
+            <Grid item>
+              <Box
+                style={{
+                  // padding: 35,
+                  backdropFilter: 'blur(5px)',
+                  // border: '2px solid black',
+                  borderRadius: 8,
+                  marginBottom: 20,
+                }}
+              >
+                <Typography variant="h2" color="primary">
+                  Be part of the future of innovative business collaboration.
+                  Join our startup studios today!
+                </Typography>
+                <Typography variant="h3">
+                  Type your email below to join our newsletter!
+                </Typography>
+              </Box>
+              <Grid container direction="row" spacing={2}>
+                <Grid item>
+                  <TextField
+                    id="outlined-basic"
+                    label="Email address"
+                    variant="outlined"
+                    style={{
+                      width: 302,
+                    }}
+                    InputProps={{
+                      endAdornment: <MailIcon />,
+                    }}
+                  />
+                </Grid>
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    style={{ height: '100%' }}
+                  >
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Hidden>
+
+        {/* <Grid
+          container
+          justify="left"
+          style={{
+            height: '100vh',
+            alignItems: 'center',
+            textAlign: 'center',
+            width: '100%',
+          }}
+        >
+          <Grid item>
+            <Box
+              style={{
+                padding: 35,
+                backdropFilter: 'blur(5px)',
+                border: '2px solid black',
+                borderRadius: 8,
+              }}
+            >
+              <Typography variant="h1" color="secondary">
+                Startup Studios
+              </Typography>
+              <Typography variant="h5">
+                {' '}
+                Stay tuned for something great.
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid> */}
+      </Layout>
+    </BackgroundImage>
   )
 }
 
-export default IndexPage
+export default withWidth()(IndexPage)
